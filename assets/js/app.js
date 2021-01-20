2e81d79b87ffa565259baac6b4c4c6e100f4b3ee
oghusky <philiposimmons@gmail.com>
2021-01-20 13:59:12 -0500

    fixing navbar when at top

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,16 @@ const emailInput = document.querySelector("#email");
 const messageInput = document.querySelector("#message");
 const navBar = document.querySelector("nav.navbar");
 
+
+window.addEventListener("load", () => {
+  navBar.classList.add("fixed-top")
+})
+
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
+  if (window.scrollY <= 4) {
+    navBar.classList.add("fixed-top");
+  }
+  else if (window.scrollY > 300) {
     navBar.classList.add("fixed-top", "darkbar");
   }
   else {
