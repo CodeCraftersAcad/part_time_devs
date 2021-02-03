3e67c24dcaa748d0c232b20d404ef324c0dc0457
oghusky <philiposimmons@gmail.com>
2021-02-03 11:13:02 -0500

    showing sessions held and ratings

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,38 @@ const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const messageInput = document.querySelector("#message");
 const navBar = document.querySelector("nav.navbar");
+const testies = document.querySelector("div#testimonials");
+const sessionsHeld = document.querySelector("#held");
+const overallRating = document.querySelector("#overall");
+const rateDivs = document.querySelector("#rate-divs");
 
+window.addEventListener("load", async () => {
+  navBar.classList.add("fixed-top");
+  const res = await fetchData("https://tranquil-reef-05344.herokuapp.com/api/ratings");
+  sessionsHeld.textContent = 10
 
-window.addEventListener("load", () => {
-  navBar.classList.add("fixed-top")
+  // rating average
+  let rate_sum = 0;
+  for (var i = 0; i < res.ratings.length; i++) {
+    rate_sum += res.ratings[i].rating;
+  }
+  overallRating.textContent = `${(rate_sum / res.ratings.length).toFixed(2)}`;
+
+  // testi divs
+  res.ratings.map((rate, i) => {
+    const align = i % 2 === 0 ? "float-start" : "float-end";
+    const border = i % 2 === 0 ? "border-right: 2px solid #fffffc" : "border-left: 2px solid #fffffc";
+    rateDivs.innerHTML += `
+      <div class="p-3 testimonial my-3 clearfix" style="${border}">
+        <p class="${align}">
+          <span class="user-rating">${rate.rating} &#x2605;</span> 
+          <span class="user-name"><em>${rate.fname} ${rate.lname}</em></span> -
+          <span class="user-comment"><b>&#8220;</b> ${rate.comment} <b>&#8221;</b></span>
+        </p>
+      </div>
+    `
+  }).slice(0, 4);
+
 })
 
 window.addEventListener("scroll", () => {
@@ -20,3 +48,8 @@ window.addEventListener("scroll", () => {
     navBar.classList.remove("fixed-top", "darkbar")
   }
 })
+
+async function fetchData(url) {
+  const data = await fetch(url);
+  return data.json();
+}
